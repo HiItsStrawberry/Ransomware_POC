b5fef7b1a9ce573818b2f7b88d32b515d5a840e5
Marcus Yau <71379913+HiItsStrawberry@users.noreply.github.com>
2022-03-01 20:49:38 +0800

    Add files via upload

---
+++ Launcher_Ransomware/ProgressReport.cs
@@ -0,0 +1,30 @@
+﻿namespace Launcher_Ransomware
+{
+    public class ProgressReport
+    {
+        public int RemainFiles { get; set; }
+        public int TotalFiles { get; set; } = 0;  
+        public int PercentageComplete { get; set; } = 0;
+        public string CurrentFilesProcessed { get; set; }
+
+        public string GetCurrentProcessedFile()
+        {
+            return $"Name: {this.CurrentFilesProcessed}";
+        }
+
+        public string GetTotalEnRemainFiles()
+        {
+            return $"Total Encrypted: {this.RemainFiles}";
+        }
+
+        public string GetTotalDeRemainFiles()
+        {
+            return $"Total Decrypted: {this.RemainFiles}";
+        }
+
+        public string GetTotalPercentageComplete()
+        {
+            return $"{this.PercentageComplete}% complete";
+        }
+    }
+}
