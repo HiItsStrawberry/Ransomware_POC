804728ac7eb65051ea89b2eb7d15a202a69bdfb8
Marcus Yau <71379913+HiItsStrawberry@users.noreply.github.com>
2022-03-03 00:14:17 +0800

    Delete Ransomware Source directory

--- Ransomware Source/FileInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Launcher_Ransomware
-{
-    public class FileInfo
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-    }
-}
--- Ransomware Source/GlobalReferences.cs
+++
@@ -1,8 +0,0 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Configuration;
-global using System.Diagnostics;
-global using System.IO;
-global using System.Threading.Tasks;
-global using System.Windows.Forms;
-global using System.Security.Cryptography;
